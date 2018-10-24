8fe62ac6f260676d20b5072a87bdd7f2725eb73b
Kenny Hwu <kennywhwu@gmail.com>
2018-10-24 10:11:43 -0700

    validated unit test for partialUpdate

--- helpers/partialUpdate.js
+++ helpers/partialUpdate.js
@@ -20,8 +20,8 @@ function sqlForPartialUpdate(table, items, key, id) {
 
   // filter out keys that start with "_" -- we don't want these in DB
   for (let key in items) {
-    if (key.startsWith("_")) {
-      delete items[key]
+    if (key.startsWith('_')) {
+      delete items[key];
     }
   }
 
@@ -31,14 +31,13 @@ function sqlForPartialUpdate(table, items, key, id) {
   }
 
   // build query
-  let cols = columns.join(", ");
+  let cols = columns.join(', ');
   let query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING *`;
 
   let values = Object.values(items);
   values.push(id);
 
-  return {query, values};
+  return { query, values };
 }
-
 
 module.exports = sqlForPartialUpdate;
