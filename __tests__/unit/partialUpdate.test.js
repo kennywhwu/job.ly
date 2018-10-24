55c8ba922074c280acd5923a00116375f9c5356c
Kenny Hwu <kennywhwu@gmail.com>
2018-10-24 10:02:50 -0700

    wrote unit test for partialUpdate

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,20 @@
-describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
+const partialUpdate = require('.../helpers/partialUpdate');
 
+describe('partialUpdate()', () => {
+  it('should generate a proper partial update query with just 1 field', function() {
     // FIXME: write real tests!
-    expect(false).toEqual(true);
-
+    // expect(false).toEqual(true);
+    expect(
+      partialUpdate(
+        'companies',
+        { description: 'Doing great!', logo_url: 'http://netflix.com' },
+        'handle',
+        'NFLX'
+      )
+    ).toEqual({
+      query:
+        'UPDATE companies SET description = $1, logo_url = $2 WHERE handle=$3 RETURNING *',
+      values: ['Doing great!', 'http://netflix.com', 'NFLX']
+    });
   });
 });
